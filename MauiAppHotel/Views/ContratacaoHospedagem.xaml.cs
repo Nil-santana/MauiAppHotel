91f967ea3cca961872d3bfaac0db743ab058aa20
Nil-santana <dimeno_@hotmail.com>
2025-10-31 12:00:01 -0300

    Atualização do projeto Cálculo diário

---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+	}
+
+    private void BtnSobre_Clicked(object sender, EventArgs e)
+    {
+
+		DisplayAlert("Desenvolvido por:", "Lenildo Santana ,2025.", "Fechar");
+    }
+}
